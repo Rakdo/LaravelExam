608fa1c653c59c4c22986a75dce355d72bf304b0
vagrant <vagrant@homestead>
2021-09-10 18:51:24 +0000

    Stable build

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contacts;
+use Illuminate\Http\Request;
+
+
+
+class ContactsController extends Controller
+{
+  
+    public function index()
+    {
+        $contactos = Contacts::all();
+        return view('welcome')-> with('contactos', $contactos);
+    }
+
+   
+    public function create()
+    {
+        
+    }
+
+   
+    public function store(Request $request)
+    {
+         $Contacto = Contacts::create([
+            'nombre' => $request->nombre ,
+            'telefono' => $request->telefono,
+            'correo' => $request->correo,
+            'estado' => 'Registro nuevo',
+        ]);
+
+         $contactos = Contacts::all();
+        return redirect()->back();
+    }
+
+  
+    public function show(Contacts $contacts)
+    {
+        
+    }
+
+    
+    public function edit(Contacts $contacts)
+    {
+        
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+      $contacto =Contacts::find($id);
+          $contacto->update([
+            'nombre' => $request->nombre ,
+            'telefono' => $request->telefono,
+            'correo' => $request->correo,
+            'estado' => $request->estado,
+            
+
+
+        ]);   
+          return redirect()->back();
+    }
+
+  
+    public function destroy($id)
+    {
+        $Contactos = Contacts::find($id);
+        $Contactos->delete();
+
+        $contact = Contacts::all();
+       return redirect()->back();
+    }
+}
